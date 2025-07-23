8e595f3e5d43278137a1b30983785e728487f1a4
CeritaBahagia-ai <ceritiaa@gmail.com>
2025-07-23 08:59:34 +0700

    pesan commit

---
+++ app/Livewire/Calmzone.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Calmzone extends Component
+{
+    public function render()
+    {
+        return view('livewire.calmzone');
+    }
+}
